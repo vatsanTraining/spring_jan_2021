0f62e741c86cd59c25537700edbb159efd66ab53
srivatsan <vatsank@gmail.com>
2021-01-10 10:08:05 +0530

    Update

--- tour-rest-api/src/main/java/com/example/demo/utils/ExceptionAdvice.java
+++ tour-rest-api/src/main/java/com/example/demo/utils/ExceptionAdvice.java
@@ -1,18 +1,24 @@
 package com.example.demo.utils;
 
+import java.time.LocalDateTime;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 
+import com.example.demo.model.ErrorDetails;
+
 @ControllerAdvice
 public class ExceptionAdvice {
 
 	
 	@ExceptionHandler(Exception.class)
-	public final ResponseEntity<String> handleAllExceptions(Exception ex, WebRequest request) {
+	public final ResponseEntity<ErrorDetails> handleAllExceptions(Exception ex, WebRequest request) {
 	          request.getDescription(false);
-	      return new ResponseEntity<>(ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+	          ErrorDetails errorDetails = new ErrorDetails(LocalDateTime.now(), ex.getMessage());
+
+	      return new ResponseEntity<>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
 	    }
 }
