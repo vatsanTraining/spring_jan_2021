a8ce9b825f6074d5dd8be89ba532bcde4db51a4f
srivatsan <vatsank@gmail.com>
2021-01-10 12:06:23 +0530

    Update

--- tour-rest-api-client/src/main/java/com/example/demo/controllers/TourClientController.java
+++ tour-rest-api-client/src/main/java/com/example/demo/controllers/TourClientController.java
@@ -1,8 +1,31 @@
 package com.example.demo.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class TourClientController {
 
+	
+	private RestTemplate template;
+
+	String baseURL = "http://localhost:6060/api/v1/tours/";
+
+	@Autowired
+	public void setTemplate(RestTemplate template) {
+		this.template = template;
+	}
+	
+	
+	@GetMapping(path = "/api/v1/client/tours")
+	public String invokeTourRestAPI() {
+		
+		
+		return this.template.getForObject(baseURL, String.class);
+		
+		
+	}
+	
 }
