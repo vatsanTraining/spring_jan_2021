86aea0fbf7e4d6bb9712f0aa8f05ce4f0d59f7e5
srivatsan <vatsank@gmail.com>
2021-01-16 12:22:20 +0530

    Update

--- customer-service/src/main/java/com/example/demo/CustomerServiceApplication.java
+++ customer-service/src/main/java/com/example/demo/CustomerServiceApplication.java
@@ -2,11 +2,13 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 
 import com.example.demo.domain.Customer;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class CustomerServiceApplication {
 
 	public static void main(String[] args) {
