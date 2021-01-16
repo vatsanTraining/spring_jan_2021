ee66a95be33026757b4e8a35b08b06b6ab186b24
srivatsan <vatsank@gmail.com>
2021-01-16 14:49:12 +0530

    Update

---
+++ invoice-service/src/main/java/com/example/demo/model/Invoice.java
@@ -0,0 +1,23 @@
+package com.example.demo.model;
+
+import org.springframework.stereotype.Component;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Component
+public class Invoice {
+
+	
+	int id;
+	String itemName;
+	double amount;
+	
+}
