c85da3f6f14f2e6712de289f3b93844cfd84258f
srivatsan <vatsank@gmail.com>
2021-01-17 12:09:56 +0530

    Update

--- product-service/src/main/java/com/example/demo/ProductServiceApplication.java
+++ product-service/src/main/java/com/example/demo/ProductServiceApplication.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 import com.example.demo.entity.Product;
 import com.example.demo.ifaces.ProductRepository;
@@ -15,7 +16,7 @@ public class ProductServiceApplication {
 		SpringApplication.run(ProductServiceApplication.class, args);
 	}
 
-	
+	@Bean
 	public CommandLineRunner runner() {
 		
 		return new CommandLineRunner() {
