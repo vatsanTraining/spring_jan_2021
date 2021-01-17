277e3270204a4928c0b0a89f29ed6c303c3b6c97
srivatsan <vatsank@gmail.com>
2021-01-17 11:49:09 +0530

    Update

--- review-service-lb-client/src/main/java/org/example/demo/MyCustomerConfiguration.java
+++ review-service-lb-client/src/main/java/org/example/demo/MyCustomerConfiguration.java
@@ -1,11 +1,10 @@
 package org.example.demo;
 
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import com.netflix.client.config.DefaultClientConfigImpl;
-import com.netflix.client.config.IClientConfig;
+
 import com.netflix.loadbalancer.IRule;
 import com.netflix.loadbalancer.RoundRobinRule;
 
