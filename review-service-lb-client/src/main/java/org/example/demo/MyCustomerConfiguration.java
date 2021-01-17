2b3b1e411a9bbcc9aeeaf78b52dd1053e1615c05
srivatsan <vatsank@gmail.com>
2021-01-17 11:26:12 +0530

    Update

--- review-service-lb-client/src/main/java/org/example/demo/MyCustomerConfiguration.java
+++ review-service-lb-client/src/main/java/org/example/demo/MyCustomerConfiguration.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.netflix.client.config.DefaultClientConfigImpl;
 import com.netflix.client.config.IClientConfig;
 import com.netflix.loadbalancer.IRule;
 import com.netflix.loadbalancer.RoundRobinRule;
@@ -12,14 +13,12 @@ import com.netflix.loadbalancer.RoundRobinRule;
 public class MyCustomerConfiguration {
 
 	
-	  @Autowired
-	  private IClientConfig config;
-
+	 
+	
 	    @Bean
-	    public IRule ribbonRule(IClientConfig config) {
+	    public IRule ribbonRule() {
 	        
-	 
-	 
+	 	 
 	          IRule rule = new RoundRobinRule();
 	               return rule;
 	              
