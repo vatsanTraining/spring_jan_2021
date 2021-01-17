3fa7cb40c0ec70004bf767daa05976a88d8ab3ea
srivatsan <vatsank@gmail.com>
2021-01-17 14:27:22 +0530

    Update

--- customer-service/src/main/java/com/example/demo/controllers/CustomerController.java
+++ customer-service/src/main/java/com/example/demo/controllers/CustomerController.java
@@ -3,6 +3,7 @@ package com.example.demo.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.domain.Customer;
@@ -25,4 +26,10 @@ public class CustomerController {
 	}
 	
 	
+	@GetMapping(path = "/api/v1/customers/{id}")
+	public Customer getCustomerById(@PathVariable("id") int id) {
+		
+		
+		return this.customer;
+	}
 }
