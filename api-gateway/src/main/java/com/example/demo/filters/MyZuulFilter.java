47e9b7117dc0e82dfabd2691f4fd92b0469c40d5
srivatsan <vatsank@gmail.com>
2021-01-17 16:37:07 +0530

    Update

--- api-gateway/src/main/java/com/example/demo/filters/MyZuulFilter.java
+++ api-gateway/src/main/java/com/example/demo/filters/MyZuulFilter.java
@@ -3,12 +3,13 @@ package com.example.demo.filters;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.context.annotation.FilterType;
+import org.springframework.stereotype.Component;
 
 import com.netflix.zuul.ZuulFilter;
 import com.netflix.zuul.constants.ZuulConstants;
 import com.netflix.zuul.context.RequestContext;
 import com.netflix.zuul.exception.ZuulException;
-
+@Component
 public class MyZuulFilter extends ZuulFilter {
 
 	@Override
@@ -22,7 +23,15 @@ public class MyZuulFilter extends ZuulFilter {
 		RequestContext ctx = RequestContext.getCurrentContext();
 		    HttpServletRequest request = ctx.getRequest();
 		    System.out.println("REQUEST METHOD: " + request.getMethod() + " Request URL : " + request.getRequestURL().toString());
-			return null;
+		
+		    
+		     if(ctx.getRequest().getRequestURI().contains("customer")) {
+		          ctx.setResponseStatusCode(400);
+		          ctx.setResponseBody("Zuul is preventing");
+		          ctx.setSendZuulResponse(false);
+		          }
+
+		    return null;
 		
 	}
 
