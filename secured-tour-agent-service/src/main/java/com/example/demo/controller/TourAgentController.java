1a490eb843eab32922e76f0d218acf318ab7baa3
srivatsan <vatsank@gmail.com>
2021-01-23 10:55:13 +0530

    Update

--- secured-tour-agent-service/src/main/java/com/example/demo/controller/TourAgentController.java
+++ secured-tour-agent-service/src/main/java/com/example/demo/controller/TourAgentController.java
@@ -1,5 +1,36 @@
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.service.TourAgentService;
+
+import java.util.*;
+import com.example.demo.entity.*;
+
+@RestController
 public class TourAgentController {
 
+	
+	
+	@Autowired
+	private TourAgentService service;
+
+
+	@GetMapping(path = "/api/v1/agents")
+	public List<TourAgent> getAll(){
+		
+		return this.service.getAll();
+	}
+
+	@GetMapping(path = "/api/v1/agents/{id}")
+	public TourAgent getAgentById(@PathVariable("id") int id){
+		
+		return this.service.getById(id).orElseThrow(()-> new RuntimeException("Element Not Found"));
+		
+	}
+	
+	
 }
--- secured-tour-agent-service/src/main/java/com/example/demo/service/TourAgentService.java
+++ secured-tour-agent-service/src/main/java/com/example/demo/service/TourAgentService.java
@@ -1,5 +1,41 @@
 package com.example.demo.service;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.repos.TourAgentRepository;
+
+
+import lombok.extern.slf4j.Slf4j;
+
+import com.example.demo.entity.*;
+
+@Service
+@Slf4j
 public class TourAgentService {
 
+	@Autowired
+	private TourAgentRepository repo;
+
+
+	
+	public List<TourAgent> getAll(){
+		
+		log.info("getAll in Tour Agent Service Called");
+		
+		return this.repo.findAll();
+	}
+	
+	
+	public Optional<TourAgent>  getById(int id) {
+		
+		log.info("getById in Tour Agent Service Called");
+		
+		return this.repo.findById(id);
+	}
+	
+	
 }
