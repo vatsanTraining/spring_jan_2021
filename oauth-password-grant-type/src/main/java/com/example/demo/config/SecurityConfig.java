a3b17ce03c386a98a54fcd1a91daa38b1b4c0ada
srivatsan <vatsank@gmail.com>
2021-01-23 15:44:28 +0530

    Update

--- oauth-password-grant-type/src/main/java/com/example/demo/config/SecurityConfig.java
+++ oauth-password-grant-type/src/main/java/com/example/demo/config/SecurityConfig.java
@@ -1,21 +1,52 @@
 package com.example.demo.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+	@Autowired
+	PasswordEncoder encoder;
+
+	
+	
+	@Override
+	public AuthenticationManager authenticationManagerBean() throws Exception {
+		return super.authenticationManagerBean();
+	}
+
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		// TODO Auto-generated method stub
-		super.configure(auth);
+		auth.inMemoryAuthentication()
+		.withUser("india")
+		    .password(encoder.encode("india"))
+		        .roles("ADMIN");
+
 	}
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		// TODO Auto-generated method stub
-		super.configure(http);
-	}
+		http
+        .authorizeRequests()
+       .antMatchers("/login").permitAll()
+       .antMatchers("/oauth/authorize")
+        .authenticated();
+      }
 
+
+
+	
+
+
+	@Bean
+	public PasswordEncoder encoder() {
+		
+		return new BCryptPasswordEncoder();
+	}
 }
