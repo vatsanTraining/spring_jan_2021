b554d79327ce94345116de4c93d911cd6568a8c7
srivatsan <vatsank@gmail.com>
2021-01-23 15:41:06 +0530

    Update

--- oauth-password-grant-type/src/main/java/com/example/demo/config/AuthorizationConfig.java
+++ oauth-password-grant-type/src/main/java/com/example/demo/config/AuthorizationConfig.java
@@ -1,6 +1,9 @@
 package com.example.demo.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
@@ -11,22 +14,35 @@ import org.springframework.security.oauth2.config.annotation.web.configurers.Aut
 @Configuration
 public class AuthorizationConfig extends AuthorizationServerConfigurerAdapter {
 
+	
+	@Autowired
+	private BCryptPasswordEncoder encoder;
+	
+	@Autowired
+	private AuthenticationManager manager;
+	
 	@Override
 	public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
-		// TODO Auto-generated method stub
-		super.configure(security);
+	
+		security.tokenKeyAccess("isAuthenticated()").checkTokenAccess("isAuthenticated()");
 	}
 
 	@Override
 	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
-		// TODO Auto-generated method stub
-		super.configure(clients);
+		
+		 clients.inMemory().withClient("ourclient")
+		      .secret(encoder.encode("pass123"))
+		         .authorizedGrantTypes("password")
+		            .scopes("read","write")
+		               .redirectUris("http://localhost:8585/login/code/ourclient");
+		 
+	
 	}
 
 	@Override
 	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
-		// TODO Auto-generated method stub
-		super.configure(endpoints);
+		
+		endpoints.authenticationManager(manager);
 	}
 
 	
